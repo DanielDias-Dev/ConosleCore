81fd3b231438b3d32bf0e319e73dfd9f4cf5a8e3
daniel-dias <danieldias.djfd@gmail.com>
2020-07-06 16:03:00 +0100

    .

--- Seção 2/ProAgil.API/Controllers/WeatherForecastController.cs
+++ Seção 2/ProAgil.API/Controllers/WeatherForecastController.cs
@@ -4,8 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using ProAgil.API.Data;
 using ProAgil.API.Model;
 //using ProAgil.Respository;
 
@@ -20,11 +21,11 @@ namespace ProAgil.API.Controllers
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<ValuesController> _logger;
+        private readonly DataContext _context;
 
-        public ValuesController(ILogger<ValuesController> logger)
+        public ValuesController(DataContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         //GET ALL
@@ -32,50 +33,54 @@ namespace ProAgil.API.Controllers
 
         public ActionResult<IEnumerable<Evento>> Get()
         {
-            return new Evento[] {
-                new Evento (){
-                    EventoId=1,
-                    Tema = "Angular e .Net Core",
-                    Local = "Belo Horizonte",
-                    Lote="1º Lote",
-                    QtdPessoas=250,
-                    DataEvento= DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento (){
-                    EventoId=2,
-                    Tema = "Angular e suas novidades",
-                    Local = "São Paulo",
-                    Lote="2º Lote",
-                    QtdPessoas=350,
-                    DataEvento= DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
 
-             };
+            return _context.Eventos.ToList();
+
+            // return new Evento[] {
+            //     new Evento (){
+            //         EventoId=1,
+            //         Tema = "Angular e .Net Core",
+            //         Local = "Belo Horizonte",
+            //         Lote="1º Lote",
+            //         QtdPessoas=250,
+            //         DataEvento= DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+            //     },
+            //     new Evento (){
+            //         EventoId=2,
+            //         Tema = "Angular e suas novidades",
+            //         Local = "São Paulo",
+            //         Lote="2º Lote",
+            //         QtdPessoas=350,
+            //         DataEvento= DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+            //     }
+
+            //  };
         }
         //GET by ID
         [HttpGet("{id}")]
 
         public ActionResult<Evento> Get(int id)
         {
-            return new Evento[] {
-                new Evento (){
-                    EventoId=1,
-                    Tema = "Angular e .Net Core",
-                    Local = "Belo Horizonte",
-                    Lote="1º Lote",
-                    QtdPessoas=250,
-                    DataEvento= DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento (){
-                    EventoId=2,
-                    Tema = "Angular e suas novidades",
-                    Local = "São Paulo",
-                    Lote="2º Lote",
-                    QtdPessoas=350,
-                    DataEvento= DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
+            return _context.Eventos.FirstOrDefault(x => x.EventoId == id);
+            // return new Evento[] {
+            //     new Evento (){
+            //         EventoId=1,
+            //         Tema = "Angular e .Net Core",
+            //         Local = "Belo Horizonte",
+            //         Lote="1º Lote",
+            //         QtdPessoas=250,
+            //         DataEvento= DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+            //     },
+            //     new Evento (){
+            //         EventoId=2,
+            //         Tema = "Angular e suas novidades",
+            //         Local = "São Paulo",
+            //         Lote="2º Lote",
+            //         QtdPessoas=350,
+            //         DataEvento= DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
+            //     }
 
-             }.FirstOrDefault(x => x.EventoId == id);
+            //  }.FirstOrDefault(x => x.EventoId == id);
         }
 
         //httpget
